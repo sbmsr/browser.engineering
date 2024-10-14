2bf5cdb563b7055e5a7660c59ddf44935a25d90b
sbmsr <sebastian.messier@gmail.com>
2024-10-14 18:46:27 -0400

    ch2-1

--- ch2/browser.py
+++ ch2/browser.py
@@ -159,13 +159,15 @@ def layout(text):
     display_list = []
     cursor_x, cursor_y = HSTEP, VSTEP
     for c in text:
+        if c == "\n":
+            cursor_y += VSTEP
+            cursor_x = HSTEP
         display_list.append((cursor_x, cursor_y, c))
         cursor_x += HSTEP
         if cursor_x >= WIDTH - HSTEP:
             cursor_y += VSTEP
             cursor_x = HSTEP
     return display_list
-
 
 if __name__ == "__main__":
     import sys
